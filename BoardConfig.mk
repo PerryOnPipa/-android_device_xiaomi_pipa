26d9c01937d81ebfdf61b14d0efd0e2a55bbcba7
kuroringo90 <ak4700@gmx.com>
2024-03-18 17:22:07 +0100

    pipa: remove kernel defconfig duplicate

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,9 +17,6 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Display
 TARGET_SCREEN_DENSITY := 400
 
-# Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/pipa.config
-
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
