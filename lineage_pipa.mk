84a0d763b2cffef842f12f2ef59f416b1d8434f0
kuroringo90 <ak4700@gmx.com>
2024-05-09 00:59:23 +0200

    pipa: rising bringup

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -8,13 +8,29 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Inherit some common lineage stuff.
+# Inherit some common Rising stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit from pipa device
 $(call inherit-product, device/xiaomi/pipa/device.mk)
 
-PRODUCT_NAME := lineage_pipa
+# Device Identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   RISING_MAINTAINER="ste0090" \
+   RISING_CHIPSET="SDM870"
+
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+TARGET_USE_GOOGLE_TELEPHONY := false
+TARGET_ENABLE_BLUR := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_DISABLE_EPPE := true
+INLINE_KERNEL_BUILDING := true
+PRODUCT_NO_CAMERA := false
+
+PRODUCT_NAME := rising_pipa
 PRODUCT_DEVICE := pipa
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
